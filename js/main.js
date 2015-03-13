3357b80b1960db7216c8f5bd8d12ebbe6d2c139d
Mystine Laframboise Vien <mystinelaframboisevien@Mystines-MacBook-Pro.local>
2015-03-13 13:48:23 -0400

    js panels

--- js/main.js
+++ js/main.js
@@ -1,16 +1,6 @@
-var $btnExpand = $('.btn-collapse-expand');
-var $panel = $('.panel');
-var $panel2 = $('.panel2');
-var $panel3 = $('.panel3');                
-
-$btnExpand.on('click', function() {
-    $panel.toggleClass('js-expand');
-})
-
-$btnExpand.on('click', function() {
-    $panel2.toggleClass('js-expand');
-})
-
-$btnExpand.on('click', function() {
-    $panel.toggleClass('js-expand');
-})
+$(document).ready(function(){
+    $('.show_hide').click(function(){
+        $(this).next('.slidingDiv').slideToggle();
+         return false;
+    });
+});
