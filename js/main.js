29689d4632be6ea525bc5ad897eee4c9eee5cf79
Mystine Laframboise Vien <mystinelaframboisevien@Mystines-MacBook-Pro.local>
2015-03-13 13:40:41 -0400

    panel javascript experiementing

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 var $btnExpand = $('.btn-collapse-expand');
 var $panel = $('.panel');
+var $panel2 = $('.panel2');
+var $panel3 = $('.panel3');                
 
 $btnExpand.on('click', function() {
     $panel.toggleClass('js-expand');
 })
 
-var $btnExpand = $('.btn-collapse-expand');
-var $panel = $('.panel');
+$btnExpand.on('click', function() {
+    $panel2.toggleClass('js-expand');
+})
 
 $btnExpand.on('click', function() {
     $panel.toggleClass('js-expand');
 })
-
