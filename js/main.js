e6d5b4aede73b6389e9af0a5fe5ff5b856b161e2
Mystine Laframboise Vien <mystinelaframboisevien@mystines-mbp.gateway.2wire.net>
2015-03-16 17:18:28 -0400

    js varitations

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+var $document = $('.slidingDiv');
+var $show_hide = $('.show_hide');
+
 $(document).ready(function(){
     $('.show_hide').click(function(){
         $(this).next('.slidingDiv').slideToggle();
