8fed700e2dfa20cad906c9c483ecf27210dd7cc7
Mystine Laframboise Vien <mystine.laframboisevien@outlook.com>
2015-04-06 23:30:52 -0400

    class changes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 var $document = $('.slidingDiv');
 var $show_hide = $('.show_hide');
 var $bus = $('.bus');
-var $secondBus = $('.second-bus');
-var $thirdBus = $('.third-bus');
 
 $(document).ready(function(){
     $('.show_hide').click(function(){
@@ -24,12 +22,4 @@ $('.history').localScroll();
 
 $bus.waypoint (function () {
   $bus.toggleClass('js-active');
-}, {offset: '50%'});
-
-$secondBus.waypoint (function () {
-  $secondBus.toggleClass('js-active');
-}, {offset: '50%'});
-
-$thirdBus.waypoint (function () {
-  $thirdBus.toggleClass('js-active');
 }, {offset: '50%'});
