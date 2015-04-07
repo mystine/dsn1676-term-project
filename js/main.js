1efa1668e366887487a74a5dd9c83323f9322843
Mystine Laframboise Vien <mystine.laframboisevien@outlook.com>
2015-04-06 22:53:53 -0400

    added js for sag animations

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ var $document = $('.slidingDiv');
 var $show_hide = $('.show_hide');
 var $btnTrigger = $('.btn-trigger');
 var $bus = $('.bus');
+var $secondBus = $('.second-bus');
+var $thirdBus = $('.third-bus');
 
 $(document).ready(function(){
     $('.show_hide').click(function(){
@@ -22,5 +24,13 @@ $('.back').localScroll();
 $('.history').localScroll();
 
 $btnTrigger.on('click', function () {
-    $bus.addClass('js-animate')
-});
+    $bus.addClass('js-animate');
+})
+
+$btnTrigger.on('click', function () {
+    $secondBus.addClass('js-animate');
+})
+
+$btnTrigger.on('click', function () {
+    $thirdBus.addClass('js-animate');
+})
